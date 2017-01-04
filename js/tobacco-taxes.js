90e1de038e5e120b1260a34139a95750ae1cf981
tvanantwerp <tom@tomvanantwerp.com>
2017-01-04 12:07:31 -0500

    Dynamic axes functional

--- js/tobacco-taxes.js
+++ js/tobacco-taxes.js
@@ -12,7 +12,7 @@ var DATA_PATH = 'data/data.min.json';
 		top: 0,
 		right: 30,
 		bottom: 30,
-		left: 30
+		left: 90
 	};
 
 	d3.json(DATA_PATH, function(error, data) {
@@ -39,7 +39,7 @@ var app = {
 			.attr('id', 'x-axis')
 			.attr(
 				'transform',
-				'translate(0, ' + (app.dimensions.height - app.padding.left) + ')'
+				'translate(0, ' + (app.dimensions.height - app.padding.bottom) + ')'
 			);
 
 		svg.append('g')
@@ -52,9 +52,7 @@ var app = {
 				'transform',
 				'translate(' + (app.dimensions.width - app.padding.right) + ', 0)');
 
-		var state = document.getElementById('stateTobaccoSelect').value;
-
-		app.draw(state);
+		app.draw();
 	},
 
 	createDropdown: function(data) {
@@ -71,26 +69,43 @@ var app = {
 	},
 
 	draw: function(state) {
-			var stateData = app.data[state];
+		var state = document.getElementById('stateTobaccoSelect').value;
+		var stateData = app.data[state];
 
-			var dateScale = d3.scaleTime()
-				.domain([new Date(Date.parse('2000-01-01')),new Date(Date.parse('2010-01-01'))])
-				.range([app.padding.left, app.dimensions.width - app.padding.right]);
+		var xMin = app.findMin(stateData.collections, 'y');
+		var xMax = app.findMax(stateData.collections, 'y');
 
-			var collectionsScale = d3.scaleLinear()
-				.domain([0,1])
-				.range([0, app.dimensions.height - app.padding.top - app.padding.bottom]);
+		var collectionsMin = app.findMin(stateData.collections, 'nCol');
+		var collectionsMax = app.findMax(stateData.collections, 'nCol');
 
-			var rateScale = d3.scaleLinear()
-				.domain([0,1])
-				.range([0, app.dimensions.height - app.padding.top - app.padding.bottom]);
+		var ratesMax = app.findMax(stateData.rates, 'rate');
 
-			var xAxis = d3.axisBottom(dateScale);
-			var yCollections = d3.axisLeft(collectionsScale);
-			var yRates = d3.axisRight(rateScale);
+		var dateScale = d3.scaleTime()
+			.domain([new Date(Date.parse(xMin + '-01-01')),new Date(Date.parse(xMax + '-12-31'))])
+			.range([app.padding.left, app.dimensions.width - app.padding.right]);
 
-			d3.select('#x-axis').call(xAxis);
-			d3.select('#y-collections').call(yCollections);
-			d3.select('#y-rates').call(yRates);
-	}
+		var collectionsScale = d3.scaleLinear()
+			.domain([collectionsMax, collectionsMin])
+			.range([0, app.dimensions.height - app.padding.top - app.padding.bottom]);
+
+		var rateScale = d3.scaleLinear()
+			.domain([ratesMax, 0])
+			.range([0, app.dimensions.height - app.padding.top - app.padding.bottom]);
+
+		var xAxis = d3.axisBottom(dateScale);
+		var yCollections = d3.axisLeft(collectionsScale);
+		var yRates = d3.axisRight(rateScale);
+
+		d3.select('#x-axis').call(xAxis);
+		d3.select('#y-collections').call(yCollections);
+		d3.select('#y-rates').call(yRates);
+	},
+
+	findMin: function(arr, obs) {
+		return Math.min.apply(Math, arr.map(function(d) { return +d[obs]; }));
+	},
+
+	findMax: function(arr, obs) {
+		return Math.max.apply(Math, arr.map(function(d) { return +d[obs]; }));
+	},
 };
