34840c2327f0e8d88a7989a5e7541dcb66852fd9
tvanantwerp <tom@tomvanantwerp.com>
2017-01-04 11:11:53 -0500

    Restructure app scaffolding

--- js/tobacco-taxes.js
+++ js/tobacco-taxes.js
@@ -1,68 +1,96 @@
+'use strict'
 
+var DATA_PATH = 'data/data.min.json';
 
-var container = document.getElementById('tobacco-taxes');
-var dimensions = container.getBoundingClientRect();
+(function() {
+	var dimensions = {
+		height: 500,
+		width: document.getElementById('tobacco-taxes').getBoundingClientRect().width
+	};
 
-var sidePadding = 30;
-var bottomPadding = 30;
-var topPadding = 0;
-var height = 500;
+	var padding = {
+		top: 0,
+		right: 30,
+		bottom: 30,
+		left: 30
+	};
+
+	d3.json(DATA_PATH, function(error, data) {
+			if (error) { console.log(error); }
+
+			app.init(data, dimensions, padding);
+	});
+})();
 
 var app = {
-	init: function(state) {
-		var data = d3.json('data/data.min.json', function(error, taxData) {
-			app.createDropdown(taxData);
-			var svg = d3.select('#tobacco-taxes')
-				.append('svg')
-				.attr('height', height)
-				.attr('width', dimensions.width);
+	init: function(data, dimensions, padding) {
+		this.data = data;
+		this.dimensions = dimensions;
+		this.padding = padding;
 
-			var dateScale = d3.scaleTime()
-				.domain([new Date(Date.parse('2000-01-01')),new Date(Date.parse('2010-01-01'))])
-				.range([sidePadding, dimensions.width - sidePadding]);
+		app.createDropdown(app.data);
 
-			var collectionsScale = d3.scaleLinear()
-				.domain([0,1])
-				.range([0, height - topPadding - bottomPadding]);
+		var svg = d3.select('#tobacco-taxes')
+			.append('svg')
+			.attr('height', app.dimensions.height)
+			.attr('width', app.dimensions.width);
 
-			var rateScale = d3.scaleLinear()
-				.domain([0,1])
-				.range([0, height - topPadding - bottomPadding]);	
+		svg.append('g')
+			.attr('id', 'x-axis')
+			.attr(
+				'transform',
+				'translate(0, ' + (app.dimensions.height - app.padding.left) + ')'
+			);
 
-			var xAxis = d3.axisBottom(dateScale);
-			var yCollections = d3.axisLeft(collectionsScale);
-			var yRate = d3.axisRight(rateScale);
+		svg.append('g')
+			.attr('id', 'y-collections')
+			.attr('transform', 'translate(' + app.padding.left + ', 0)');
 
-			svg.append('g')
-				.attr(
-					'transform',
-					'translate(0, ' + (height - sidePadding) + ')'
-				)
-				.call(xAxis);
+		svg.append('g')
+			.attr('id', 'y-rates')
+			.attr(
+				'transform',
+				'translate(' + (app.dimensions.width - app.padding.right) + ', 0)');
 
-			svg.append('g')
-				.attr('transform', 'translate(' + sidePadding + ', 0)')
-				.call(yCollections);
+		var state = document.getElementById('stateTobaccoSelect').value;
 
-			svg.append('g')
-				.attr(
-					'transform',
-					'translate(' + (dimensions.width - sidePadding) + ', 0)'
-				)
-				.call(yRate);
-			}
-		);
+		app.draw(state);
 	},
+
 	createDropdown: function(data) {
 		d3.select('#tobacco-taxes').append('select')
-			.attr('id', 'stateTobaccoSelect');
+			.attr('id', 'stateTobaccoSelect')
+			.attr('onchange', 'app.draw()');
+
 		var keys = Object.keys(data);
 		for (var key in data) {
 			d3.select('#stateTobaccoSelect').append('option')
 				.attr('value', key)
 				.html(data[key].name);
 		}
+	},
+
+	draw: function(state) {
+			var stateData = app.data[state];
+
+			var dateScale = d3.scaleTime()
+				.domain([new Date(Date.parse('2000-01-01')),new Date(Date.parse('2010-01-01'))])
+				.range([app.padding.left, app.dimensions.width - app.padding.right]);
+
+			var collectionsScale = d3.scaleLinear()
+				.domain([0,1])
+				.range([0, app.dimensions.height - app.padding.top - app.padding.bottom]);
+
+			var rateScale = d3.scaleLinear()
+				.domain([0,1])
+				.range([0, app.dimensions.height - app.padding.top - app.padding.bottom]);
+
+			var xAxis = d3.axisBottom(dateScale);
+			var yCollections = d3.axisLeft(collectionsScale);
+			var yRates = d3.axisRight(rateScale);
+
+			d3.select('#x-axis').call(xAxis);
+			d3.select('#y-collections').call(yCollections);
+			d3.select('#y-rates').call(yRates);
 	}
 };
-
-app.init();
